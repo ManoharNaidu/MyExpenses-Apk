fb717f0086359a19a92f472f1e3c6d7433a9c3e6
Manohar Naidu Bheesetti <beesettim27@gmail.com>
2026-02-04 15:49:32 +1100

    added .env

--- my_expenses/lib/data/supabase_client.dart
+++ my_expenses/lib/data/supabase_client.dart
@@ -1,14 +1,12 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SupabaseClientManager {
-  static const String supabaseUrl = 'YOUR_SUPABASE_URL';
-  static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
-
   static Future<void> initialize() async {
+    await dotenv.load(fileName: ".env");
     await Supabase.initialize(
-      url: 'https://egqlppxwewqfjzidtbky.supabase.co',
-      anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVncWxwcHh3ZXdxZmp6aWR0Ymt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzNTA4NzQsImV4cCI6MjA4MzkyNjg3NH0._Az8bLuuS0VTR91mQAt-s-2A6cAFLd_IWwtvuAMlRpw',
+      url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
     );
   }
 
